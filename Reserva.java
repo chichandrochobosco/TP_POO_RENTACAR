3d0569bc3512a76c1bb41b11df24cbc50627d55d
AgusFuentes17 <112353371+AgusFuentes17@users.noreply.github.com>
2023-06-22 19:37:49 -0300

    Update Reserva.java

--- Reserva.java
+++ Reserva.java
@@ -1,42 +1,109 @@
-
 package rentacarloboscofuentesfresco;
 
+import java.time.LocalDate;
+import java.time.Period;
 import java.util.ArrayList;
-import java.util.Date;
-
 
 public class Reserva {
-    private ArrayList<Auto> listaAutos = new ArrayList<>();
-    private Cliente cliente;
-    private Date fechaIni;
-    private Date fechaFin;
+    private ArrayList<Auto> lista;
+    private Cliente cli;
+    private LocalDate fechaIni;
+    private LocalDate fechaFin;
     private int cantDias;
-    private int idReserva;
-
-    public Reserva(Cliente cliente, int cantDias) {
-        this.cliente = cliente;
-        this.cantDias = cantDias;
-    }
-
-    public Reserva(Cliente cliente, Date fechaFin) {
-        this.cliente = cliente;
-        this.fechaFin = fechaFin;
+    private static int cantRes;
+    private int idRes;
+    
+    public RENTA(Cliente c, int dias){
+        this.cli = c;
+        this.cantDias = dias;
+        this.cantRes ++;
+        this.idRes = cantRes;
+        this.lista = new ArrayList();
     }
     
-    public void agregarAuto(){
-        
+    public RENTA(CLIENTE c, LocalDate fechaFin){
+        this.cli = c;
+        this.fechaFin = fechaFin;
+        this.cantRes ++;
+        this.idRes = cantRes;
     }
+    
+    public ArrayList<Auto> getLista() {
+        return lista;
+    }
+
+    public void setLista(ArrayList<Auto> lista) {
+        this.lista = lista;
+    }
+
+    public Cliente getCli() {
+        return cli;
+    }
+
+    public void setCli(Cliente cli) {
+        this.cli = cli;
+    }
+
+    public LocalDate getFechaIni() {
+        return fechaIni;
+    }
+
+    public void setFechaIni(LocalDate fechaIni) {
+        this.fechaIni = fechaIni;
+    }
+
+    public LocalDate getFechaFin() {
+        return fechaFin;
+    }
+
+    public void setFechaFin(LocalDate fechaFin) {
+        this.fechaFin = fechaFin;
+    }
+
+    public int getCantDias() {
+        return cantDias;
+    }
+
+    public void setCantDias(int canntDias) {
+        this.cantDias = canntDias;
+    }
+
+    public int getIdRes() {
+        return idRes;
+    }
+
+    public void setIdRes(int idRes) {
+        this.idRes = idRes;
+    }
+
+    public static int getCantRes() {
+        return cantRes;
+    }
+
+    public static void setCantRes(int cantRes) {
+        Reserva.cantRes = cantRes;
+    }
+    
+    public void agregarAuto(Auto a){
+        lista.add(a);
+    }
+    
     public int calcularDias(){
-        return 0;
+        Period periodoDias = fechaIni.until(fechaFin);
+        int dias = periodoDias.getDays();
+        return dias;
     }
+    
     public void calcularFechaFin(){
         
     }
+    
     public double calcularTotalRenta(){
-        return 0;
-    } 
-    public void imprimirTicket(){
         
     }
     
+    public void imprimirTicket(){
+        
+    }
+
 }
