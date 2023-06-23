cb4a66e32fbec7ff92c978cf167bcd8dfffb7e8a
AgusFuentes17 <112353371+AgusFuentes17@users.noreply.github.com>
2023-06-23 08:49:57 -0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,20 +1,29 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package rentacarloboscofuentesfresco;
 
-/**
- *
- * @author Licha
- */
 public class Main {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Cliente c1 = new Cliente("Pedro", 1234567, "Galvan 2099", 12345678);
+        Auto a1 = new Auto("ABC123", "Fiat 1", "Fiat", "Amarillo", 999999);
+        Auto a2 = new Auto("WYZ789", "Ford Ka", "Ford", "Violeta", 777777);
+        
+        Cliente c2 = new Cliente("Chicha", 9876543, "Larralde 1916", 76833320);
+        Auto a3 = new Auto("DEF456", "Camaro", "Chevrolet", "Negro", 456123);
+        Auto a4 = new Auto("GHI437", "Roadster", "Tesla", "Amarillo", 4444444);
+        
+        Reserva r1 = new Reserva(c1, 23);
+        r1.agregarAuto(a1);
+        r1.agregarAuto(a2);
+        
+        r1.calcularFechaFin();
+        r1.imprimirTicket();
+        
+        Reserva r2 = new Reserva(c2, "2023-07-01");
+        r2.agregarAuto(a3);
+        r2.agregarAuto(r4);
+        
+        r2.calcularDias();
+        r2.imprimirTicket();
     }
     
 }
